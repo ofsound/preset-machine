edc2117ba1dc680ba141199b2e721659ead0e9a2
Ben Montgomery <ben@modernthings.net>
2025-08-05 14:35:33 -0600

    Remove computed/corrected, just watch actives

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, computed } from 'vue'
+import { ref, watch } from 'vue'
 
 import AdjustmentSlider from './components/AdjustmentSlider.vue'
 import JsonDrop from './components/JsonDrop.vue'
@@ -18,19 +18,10 @@ const activeOscTotal = 8
 const activeOffsets = ref<number[]>(new Array(activeOscTotal).fill(null))
 const activeHolds = ref<number[]>(new Array(activeOscTotal).fill(null))
 
-const correctedOffsets = computed(() => {
-  return activeOffsets.value.map((item) => item / 90)
-})
-
-const correctedHolds = computed(() => {
-  return activeHolds.value.map((item) => item / 90)
-})
-
-const selectedValue = ref<number>()
-
 const tempo = ref<number>(120)
 
-const options = ref([
+const toolOptionValue = ref<number>()
+const toolOptions = ref([
   { text: '8th', value: 2 },
   { text: 'Dotted 8th', value: 3 },
   { text: '1/4', value: 4 },
@@ -38,10 +29,12 @@ const options = ref([
   { text: 'drag', value: 1 },
 ])
 
-watch(correctedOffsets, () => {
+watch([activeOffsets, activeHolds], () => {
   if (jsonData.value) {
-    jsonData.value.offsets.splice(0, 8, ...correctedOffsets.value)
-    jsonData.value.holds.splice(0, 8, ...correctedHolds.value)
+    const correctedOffsets = activeOffsets.value.map((item) => item / 90)
+    const correctedHolds = activeHolds.value.map((item) => item / 90)
+    jsonData.value.offsets.splice(0, activeOscTotal, ...correctedOffsets)
+    jsonData.value.holds.splice(0, activeOscTotal, ...correctedHolds)
   }
 })
 </script>
@@ -61,17 +54,17 @@ watch(correctedOffsets, () => {
       <AdjustmentSlider
         v-for="(n, i) in activeOscTotal"
         :key="n"
+        :value="toolOptionValue"
         v-model:offset="activeOffsets[i]"
         v-model:hold="activeHolds[i]"
-        v-model:value="selectedValue"
       />
     </div>
 
     <input type="text" v-model="tempo" />
 
-    <select id="myDropdown" v-model="selectedValue">
+    <select id="myDropdown" v-model="toolOptionValue">
       <option value="" disabled>Please select one</option>
-      <option v-for="option in options" :key="option.value" :value="option.value">
+      <option v-for="option in toolOptions" :key="option.value" :value="option.value">
         {{ option.text }}
       </option>
     </select>
