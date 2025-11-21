49330c9f04f28155d68e6c0adc6f035aea863f9e
Ben Montgomery <ben@modernthings.net>
2025-11-20 20:19:45 -0700

    Call the different sections Modules

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import JsonSave from '@/components/JsonSave.vue'
-// import OffsetHold from '@/components/OffsetHold.vue'
-// import FreeformDraw from '@/components/FreeformDraw.vue'
-import RandomizeEnvelopes from '@/components/RandomizeEnvelopes.vue'
 
 import { useStore } from '@/stores/store'
-import PageTwo from './components/PageTwo.vue'
-
 const store = useStore()
+
+import PageTwo from './components/PageTwo.vue'
+import OffsetHoldModule from '@/components/OffsetHoldModule.vue'
+import FreeformModule from '@/components/FreeformModule.vue'
+import RandomizeModule from '@/components/RandomizeModule.vue'
 </script>
 
 <template>
@@ -19,19 +19,13 @@ const store = useStore()
       <JsonSave />
     </div>
 
-    <!-- <div class="flex">
-      <FreeformDraw :segment="store.preset.offsets" />
-      <FreeformDraw :segment="store.preset.attacks" />
-      <FreeformDraw :segment="store.preset.decays" />
-      <FreeformDraw :segment="store.preset.holds" />
-      <FreeformDraw :segment="store.preset.releases" />
-    </div> -->
-
     <PageTwo />
 
-    <RandomizeEnvelopes />
+    <OffsetHoldModule />
 
-    <!-- <OffsetHold /> -->
+    <FreeformModule />
+
+    <RandomizeModule />
 
     <footer class="mt-auto flex justify-between pt-3 pb-3 text-right text-xs">
       <!-- <div class="italic">click (and drag) on row to choose an offset and hold length</div> -->
---
+++ src/components/FreeformModule.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import FreeformDraw from '@/components/FreeformDraw.vue'
+
+import { useStore } from '@/stores/store'
+const store = useStore()
+</script>
+
+<template>
+  <div class="flex">
+    <FreeformDraw :segment="store.preset.offsets" />
+    <FreeformDraw :segment="store.preset.attacks" />
+    <FreeformDraw :segment="store.preset.decays" />
+    <FreeformDraw :segment="store.preset.holds" />
+    <FreeformDraw :segment="store.preset.releases" />
+  </div>
+</template>
