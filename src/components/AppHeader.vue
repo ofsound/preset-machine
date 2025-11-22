1dc48c63b93a319a0bdcd8d647b76873e7f875f7
Ben Montgomery <ben@modernthings.net>
2025-11-21 21:46:24 -0700

    Preset Name and author now editable

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import JsonSave from '@/components/JsonSave.vue'
-
 import { useStore } from '@/stores/store'
+
+import JsonSave from '@/components/JsonSave.vue'
 
 const store = useStore()
 </script>
@@ -9,7 +9,20 @@ const store = useStore()
 <template>
   <div class="mb-4 flex items-end justify-between">
     <h1 class="py-4 text-center text-3xl font-bold">
-      {{ store.preset.name }} : {{ store.preset.author }}
+      <input
+        type="text"
+        id="preset-name"
+        name="preset-name"
+        v-model="store.preset.name"
+        class="border p-1 font-bold"
+      />
+      <input
+        type="text"
+        id="preset-author"
+        name="preset-author"
+        v-model="store.preset.author"
+        class="border p-1 font-bold"
+      />
     </h1>
     <JsonSave />
   </div>
